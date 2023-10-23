70d07e961b05847cda60e5fbec1e006fd8a5573d
Juliana Karoline <julianaklulo@gmail.com>
2023-10-23 11:30:28 -0300

    Update navbar

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,9 +2,12 @@ AUTHOR = 'PyLadies São Carlos'
 SITENAME = 'PyLadies São Carlos'
 SITE_DESCRIPTION = 'PyLadies São Carlos é um grupo de mulheres que se reúne para aprender a programar em Python, compartilhar conhecimento e promover a inclusão de mulheres na tecnologia.'
 
+AUTHOR_EMAIL = 'saocarlos@pyladies.com'
 AUTHOR_TWITTER = 'pyladiessanca'
 AUTHOR_FACEBOOK = 'pyladiessanca'
 AUTHOR_GITHUB = 'pyladiessanca'
+AUTHOR_INSTAGRAM = 'pyladiessanca'
+AUTHOR_LINKEDIN = 'pyladiessanca'
 
 PATH = 'content'
 
@@ -24,20 +27,8 @@ THEME = 'themes/pelican-mediumfox'
 
 SUMMARY_MAX_LENGTH = 30
 
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
 
 MENUITEMS = [
     ('Sobre', './index.html'),
